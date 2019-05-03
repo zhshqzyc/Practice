327dcafac174290d9f1278abe39350b292b873ce
Hui Zhao <zhshqzyc@gmail.com>
2019-05-03 08:36:59 -0400

    Add files via upload
    You a given a number N. You need to print the pattern for the given value of N.
                for N=2 the pattern will be 
                2 2 1 1
                2 1
                for N=3 the pattern will be 
                3 3 3 2 2 2 1 1 1
                3 3 2 2 1 1
                3 2 1
    
                Input Format:
                The first line of input is the number of testcases T. The T test cases follow. The first line of each test case is an integer N.
    
                Output Format:
                For each test case, in a new line, print the required pattern in a singleline . 
                Note : Instead of printing new line print a "$" without quotes.
    
                Your Task:
                Since this is a function problem, you don't need to worry about the testcases. Your task is to complete the function printPat which takes one argument 'N' denoting the length of the pattern.
    
                Constraints:
                1 <= T <= 20
                1 <= N <= 40
    
                Example:
                Input
                2
                2
                3
                Output
                2 2 1 1 $2 1 $
                3 3 3 2 2 2 1 1 1 $3 3 2 2 1 1 $3 2 1 $

---
+++ PrintPattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrintPattern
+{
+    class PrintPattern
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int m = n;
+            while (m > 0)
+            {
+                PrintNumbers(n,m);
+                Console.Write("$");
+                m--;
+            }
+            Console.WriteLine(); 
+        }
+
+        static void PrintNumbers(int n, int m)
+        {
+            int length = m;
+            while (n > 0)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write(n + " ");
+                }
+                n--;
+            }
+        }
+    }
+}
