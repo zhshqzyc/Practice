e8c60c1b70876b383ea3628a95492e1a269a50ca
Hui Zhao <zhshqzyc@gmail.com>
2021-12-28 10:47:21 -0500

    Add files via upload

---
+++ StringPalindrome.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+string s = "aabbaa";
+Console.WriteLine(ifStringPalindrome(s));
+bool ifStringPalindrome(string s)
+{
+    if (s == null) return false;
+    for (int i = 0; i < s.Length / 2; i++)
+    {
+        if (s[i] != s[s.Length - 1 - i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
