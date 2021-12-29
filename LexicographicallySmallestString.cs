5b9fff0cca09e7eff532418052bc6f9940c24e7b
Hui Zhao <zhshqzyc@gmail.com>
2021-12-29 09:43:36 -0500

    Add files via upload

---
+++ LexicographicallySmallestString.cs
@@ -0,0 +1,10 @@
+﻿string s = "abcda";
+int i = 0;
+for (; i < s.Length - 1; i++)
+{
+    if (s[i] > s[i + 1])
+    {
+        break;
+    }
+}
+Console.WriteLine(s.Substring(0, i) + s.Substring(i + 1, s.Length - i - 1));
